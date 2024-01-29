8ceb6ce04380881a98cc835f7534ae2e9ee5f0d0
nicorey89 <nicolasthian8907@gmail.com>
2024-01-29 20:54:55 -0300

    creando context y agregando la card de pacientes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
-    return (
-      <h1 className="font-black text-5xl text-center  mx-auto">Seguimiento Pacientes <span className="text-indigo-700">Veterinaria</span></h1>
-  
-    )
-  }
+  return (
+    <h1 className="font-black text-5xl text-center  mx-auto">Seguimiento Pacientes <span className="text-indigo-700">Veterinaria</span></h1>
+
+  )
+}
